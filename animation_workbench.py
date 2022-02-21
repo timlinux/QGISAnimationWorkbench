a1c66d16b3de911ec00199f9bf2a38500df54e7d
Tim Sutton <tim@kartoza.com>
2022-02-21 16:53:51 +0000

    updated gui class

--- animation_workbench.py
+++ animation_workbench.py
@@ -5,9 +5,119 @@ import os
 import timeit
 import time
 
-from PyQt5.QtGui import QImage, QPainter
+from PyQt5.QtGui import QImage, QPainter, QtWidgets
 from PyQt5.QtCore import QEasingCurve
+from PyQt5 import QtCore
 from qgis.core import QgsPointXY
+
+
+
+def resources_path(*args):
+    """Get the path to our resources folder.
+
+    .. versionadded:: 3.0
+
+    Note that in version 3.0 we removed the use of Qt Resource files in
+    favour of directly accessing on-disk resources.
+
+    :param args List of path elements e.g. ['img', 'logos', 'image.png']
+    :type args: str
+
+    :return: Absolute path to the resources folder.
+    :rtype: str
+    """
+    path = os.path.dirname(__file__)
+    path = os.path.abspath(
+        os.path.join(path, os.path.pardir, 'resources'))
+    for item in args:
+        path = os.path.abspath(os.path.join(path, item))
+
+    return path
+
+
+def resource_url(path):
+    """Get the a local filesystem url to a given resource.
+
+    .. versionadded:: 3.0
+
+    Note that we dont use Qt Resource files in
+    favour of directly accessing on-disk resources.
+
+    :param path: Path to resource e.g. /home/timlinux/foo/bar.png
+    :type path: str
+
+    :return: A valid file url e.g. file:///home/timlinux/foo/bar.png
+    :rtype: str
+    """
+    url = QtCore.QUrl.fromLocalFile(path)
+    return str(url.toString())
+
+
+def get_ui_class(ui_file):
+    """Get UI Python class from .ui file.
+
+       Can be filename.ui or subdirectory/filename.ui
+
+    :param ui_file: The file of the ui in safe.gui.ui
+    :type ui_file: str
+    """
+    os.path.sep.join(ui_file.split('/'))
+    ui_file_path = os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            os.pardir,
+            'gui',
+            'ui',
+            ui_file
+        )
+    )
+    return uic.loadUiType(ui_file_path)[0]
+
+FORM_CLASS = get_ui_class('animation_workbench_base.ui')
+
+
+class MultiBufferDialog(QtWidgets.QDialog, FORM_CLASS):
+    """Dialog implementation class Animation Workbench class."""
+
+    def __init__(self, parent=None, iface=None, dock_widget=None):
+        """Constructor for the multi buffer dialog.
+
+        :param parent: Parent widget of this dialog.
+        :type parent: QWidget
+        """
+        QtWidgets.QDialog.__init__(self, parent)
+        self.setupUi(self)
+        self.setWindowTitle(self.tr('InaSAFE Multi Buffer Tool'))
+        icon = resources_path('img', 'icons', 'show-multi-buffer.svg')
+        self.setWindowIcon(QtGui.QIcon(icon))
+        self.parent = parent
+        self.iface = iface
+        # Set up things for context help
+        self.help_button = self.button_box.button(
+            QtWidgets.QDialogButtonBox.Help)
+        # Allow toggling the help button
+        self.help_button.setCheckable(True)
+        self.help_button.toggled.connect(self.help_toggled)
+        self.main_stacked_widget.setCurrentIndex(1)
+
+        # Fix for issue 1699 - cancel button does nothing
+        cancel_button = self.button_box.button(
+            QtWidgets.QDialogButtonBox.Cancel)
+        cancel_button.clicked.connect(self.reject)
+        # Fix ends
+        ok_button = self.button_box.button(QtWidgets.QDialogButtonBox.Ok)
+        ok_button.clicked.connect(self.accept)
+
+    def accept(self):
+        """Process the layer for multi buffering and generate a new layer.
+
+        .. note:: This is called on OK click.
+        """
+        # set parameter from dialog
+
+
+
+
 
 def initialize_debugger():
     import multiprocessing
