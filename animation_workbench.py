cca68c5a47fe69e9c3ff1ddcf8e2ffababdaacd6
Tim Sutton <tim@kartoza.com.com>
2022-02-27 01:30:09 +0000

    wired up first prototype of easing previews

--- animation_workbench.py
+++ animation_workbench.py
@@ -12,7 +12,7 @@ import qgis  # NOQA
 from qgis.PyQt import QtGui, QtWidgets
 from qgis.PyQt.QtGui import QImage, QPainter
 from qgis.PyQt import uic
-from qgis.PyQt.QtCore import QEasingCurve
+from qgis.PyQt.QtCore import QEasingCurve, QPropertyAnimation, QPoint
 from qgis.PyQt import QtCore
 from qgis.core import (
     QgsPointXY,
@@ -173,6 +173,130 @@ class AnimationWorkbench(QtWidgets.QDialog, FORM_CLASS):
         self.map_mode = MapMode.SPHERE
         self.easing_mode = EasingMode.EASE_OUT
 
+        # Perhaps we can softcode these items using the logic here
+        # https://github.com/baoboa/pyqt5/blob/master/examples/animation/easing/easing.py#L159
+        self.pan_easing_combo.addItem("Linear",QEasingCurve.Linear)
+        self.pan_easing_combo.addItem("InQuad",QEasingCurve.InQuad)
+        self.pan_easing_combo.addItem("OutQuad",QEasingCurve.OutQuad)
+        self.pan_easing_combo.addItem("InOutQuad",QEasingCurve.InOutQuad)
+        self.pan_easing_combo.addItem("OutInQuad",QEasingCurve.OutInQuad)
+        self.pan_easing_combo.addItem("InCubic",QEasingCurve.InCubic)
+        self.pan_easing_combo.addItem("OutCubic",QEasingCurve.OutCubic)
+        self.pan_easing_combo.addItem("InOutCubic",QEasingCurve.InOutCubic)
+        self.pan_easing_combo.addItem("OutInCubic",QEasingCurve.OutInCubic)
+        self.pan_easing_combo.addItem("InQuart",QEasingCurve.InQuart)
+        self.pan_easing_combo.addItem("OutQuart",QEasingCurve.OutQuart)
+        self.pan_easing_combo.addItem("InOutQuart",QEasingCurve.InOutQuart)
+        self.pan_easing_combo.addItem("OutInQuart",QEasingCurve.OutInQuart)
+        self.pan_easing_combo.addItem("InQuint",QEasingCurve.InQuint)
+        self.pan_easing_combo.addItem("OutQuint",QEasingCurve.OutQuint)
+        self.pan_easing_combo.addItem("InOutQuint",QEasingCurve.InOutQuint)
+        self.pan_easing_combo.addItem("OutInQuint",QEasingCurve.OutInQuint)
+        self.pan_easing_combo.addItem("InSine",QEasingCurve.InSine)
+        self.pan_easing_combo.addItem("OutSine",QEasingCurve.OutSine)
+        self.pan_easing_combo.addItem("InOutSine",QEasingCurve.InOutSine)
+        self.pan_easing_combo.addItem("OutInSine",QEasingCurve.OutInSine)
+        self.pan_easing_combo.addItem("InExpo",QEasingCurve.InExpo)
+        self.pan_easing_combo.addItem("OutExpo",QEasingCurve.OutExpo)
+        self.pan_easing_combo.addItem("InOutExpo",QEasingCurve.InOutExpo)
+        self.pan_easing_combo.addItem("OutInExpo",QEasingCurve.OutInExpo)
+        self.pan_easing_combo.addItem("InCirc",QEasingCurve.InCirc)
+        self.pan_easing_combo.addItem("OutCirc",QEasingCurve.OutCirc)
+        self.pan_easing_combo.addItem("InOutCirc",QEasingCurve.InOutCirc)
+        self.pan_easing_combo.addItem("OutInCirc",QEasingCurve.OutInCirc)
+        self.pan_easing_combo.addItem("InElastic",QEasingCurve.InElastic)
+        self.pan_easing_combo.addItem("OutElastic",QEasingCurve.OutElastic)
+        self.pan_easing_combo.addItem("InOutElastic",QEasingCurve.InOutElastic)
+        self.pan_easing_combo.addItem("OutInElastic",QEasingCurve.OutInElastic)
+        self.pan_easing_combo.addItem("InBack",QEasingCurve.InBack)
+        self.pan_easing_combo.addItem("OutBack",QEasingCurve.OutBack)
+        self.pan_easing_combo.addItem("InOutBack",QEasingCurve.InOutBack)
+        self.pan_easing_combo.addItem("OutInBack",QEasingCurve.OutInBack)
+        self.pan_easing_combo.addItem("InBounce",QEasingCurve.InBounce)
+        self.pan_easing_combo.addItem("OutBounce",QEasingCurve.OutBounce)
+        self.pan_easing_combo.addItem("InOutBounce",QEasingCurve.InOutBounce)
+        self.pan_easing_combo.addItem("OutInBounce",QEasingCurve.OutInBounce)
+        self.pan_easing_combo.addItem("BezierSpline",QEasingCurve.BezierSpline)
+        self.pan_easing_combo.addItem("TCBSpline",QEasingCurve.TCBSpline)
+        self.pan_easing_combo.addItem("Custom",QEasingCurve.Custom)
+        
+        self.zoom_easing_combo.addItem("Linear",QEasingCurve.Linear)
+        self.zoom_easing_combo.addItem("InQuad",QEasingCurve.InQuad)
+        self.zoom_easing_combo.addItem("OutQuad",QEasingCurve.OutQuad)
+        self.zoom_easing_combo.addItem("InOutQuad",QEasingCurve.InOutQuad)
+        self.zoom_easing_combo.addItem("OutInQuad",QEasingCurve.OutInQuad)
+        self.zoom_easing_combo.addItem("InCubic",QEasingCurve.InCubic)
+        self.zoom_easing_combo.addItem("OutCubic",QEasingCurve.OutCubic)
+        self.zoom_easing_combo.addItem("InOutCubic",QEasingCurve.InOutCubic)
+        self.zoom_easing_combo.addItem("OutInCubic",QEasingCurve.OutInCubic)
+        self.zoom_easing_combo.addItem("InQuart",QEasingCurve.InQuart)
+        self.zoom_easing_combo.addItem("OutQuart",QEasingCurve.OutQuart)
+        self.zoom_easing_combo.addItem("InOutQuart",QEasingCurve.InOutQuart)
+        self.zoom_easing_combo.addItem("OutInQuart",QEasingCurve.OutInQuart)
+        self.zoom_easing_combo.addItem("InQuint",QEasingCurve.InQuint)
+        self.zoom_easing_combo.addItem("OutQuint",QEasingCurve.OutQuint)
+        self.zoom_easing_combo.addItem("InOutQuint",QEasingCurve.InOutQuint)
+        self.zoom_easing_combo.addItem("OutInQuint",QEasingCurve.OutInQuint)
+        self.zoom_easing_combo.addItem("InSine",QEasingCurve.InSine)
+        self.zoom_easing_combo.addItem("OutSine",QEasingCurve.OutSine)
+        self.zoom_easing_combo.addItem("InOutSine",QEasingCurve.InOutSine)
+        self.zoom_easing_combo.addItem("OutInSine",QEasingCurve.OutInSine)
+        self.zoom_easing_combo.addItem("InExpo",QEasingCurve.InExpo)
+        self.zoom_easing_combo.addItem("OutExpo",QEasingCurve.OutExpo)
+        self.zoom_easing_combo.addItem("InOutExpo",QEasingCurve.InOutExpo)
+        self.zoom_easing_combo.addItem("OutInExpo",QEasingCurve.OutInExpo)
+        self.zoom_easing_combo.addItem("InCirc",QEasingCurve.InCirc)
+        self.zoom_easing_combo.addItem("OutCirc",QEasingCurve.OutCirc)
+        self.zoom_easing_combo.addItem("InOutCirc",QEasingCurve.InOutCirc)
+        self.zoom_easing_combo.addItem("OutInCirc",QEasingCurve.OutInCirc)
+        self.zoom_easing_combo.addItem("InElastic",QEasingCurve.InElastic)
+        self.zoom_easing_combo.addItem("OutElastic",QEasingCurve.OutElastic)
+        self.zoom_easing_combo.addItem("InOutElastic",QEasingCurve.InOutElastic)
+        self.zoom_easing_combo.addItem("OutInElastic",QEasingCurve.OutInElastic)
+        self.zoom_easing_combo.addItem("InBack",QEasingCurve.InBack)
+        self.zoom_easing_combo.addItem("OutBack",QEasingCurve.OutBack)
+        self.zoom_easing_combo.addItem("InOutBack",QEasingCurve.InOutBack)
+        self.zoom_easing_combo.addItem("OutInBack",QEasingCurve.OutInBack)
+        self.zoom_easing_combo.addItem("InBounce",QEasingCurve.InBounce)
+        self.zoom_easing_combo.addItem("OutBounce",QEasingCurve.OutBounce)
+        self.zoom_easing_combo.addItem("InOutBounce",QEasingCurve.InOutBounce)
+        self.zoom_easing_combo.addItem("OutInBounce",QEasingCurve.OutInBounce)
+        self.zoom_easing_combo.addItem("BezierSpline",QEasingCurve.BezierSpline)
+        self.zoom_easing_combo.addItem("TCBSpline",QEasingCurve.TCBSpline)
+        self.zoom_easing_combo.addItem("Custom",QEasingCurve.Custom)
+        # Set up easing previews
+        self.pan_easing_preview_icon = QtWidgets.QWidget(self.pan_easing_preview)
+        self.pan_easing_preview_icon.setStyleSheet("background-color:yellow;border-radius:5px;")
+        self.pan_easing_preview_icon.resize(10, 10)
+        self.pan_easing_preview_animation = QPropertyAnimation(self.pan_easing_preview, b"pos")
+        self.pan_easing_preview_animation.setEasingCurve(QEasingCurve.InOutCubic)
+        self.pan_easing_preview_animation.setStartValue(QPoint(0, 0))
+        self.pan_easing_preview_animation.setEndValue(QPoint(150, 150))
+        self.pan_easing_preview_animation.setDuration(1500)
+        # loop forever ...
+        self.pan_easing_preview_animation.setLoopCount(-1)
+        self.pan_easing_preview_animation.start()
+
+        self.zoom_easing_preview_icon = QtWidgets.QWidget(self.zoom_easing_preview)
+        self.zoom_easing_preview_icon.setStyleSheet("background-color:green;border-radius:5px;")
+        self.zoom_easing_preview_icon.resize(10, 10)
+        self.zoom_easing_preview_animation = QPropertyAnimation(self.zoom_easing_preview, b"pos")
+        self.zoom_easing_preview_animation.setEasingCurve(QEasingCurve.InOutCubic)
+        self.zoom_easing_preview_animation.setStartValue(QPoint(0, 0))
+        self.zoom_easing_preview_animation.setEndValue(QPoint(150, 150))
+        self.zoom_easing_preview_animation.setDuration(1500)
+        # loop forever ...
+        self.zoom_easing_preview_animation.setLoopCount(-1)
+        self.zoom_easing_preview_animation.start()
+
+        # Keep this after above animations are set up 
+        # since the slot requires the above setup to be completed
+        self.pan_easing_combo.currentIndexChanged.connect(
+            self.pan_easing_changed)
+        self.zoom_easing_combo.currentIndexChanged.connect(
+            self.zoom_easing_changed)
+
+
         # The maximum number of concurrent threads to allow
         # during rendering. Probably setting to the same number 
         # of CPU cores you have would be a good conservative approach
@@ -180,6 +304,36 @@ class AnimationWorkbench(QtWidgets.QDialog, FORM_CLASS):
         self.render_thread_pool_size = 100
         # Number of currently running render threads
         self.current_render_thread_count = 0
+
+    def pan_easing_changed(self, index):
+        """Handle changes to the pan easing type combo.
+        
+        .. note:: This is called on changes to the pan easing combo.
+
+        .. versionadded:: 1.0
+
+        :param index: Index of the now selected combo item.
+        :type flag: int
+
+        """
+        easing = QEasingCurve.Type(index)
+        self.pan_easing_preview_animation.setEasingCurve(easing)
+
+    def zoom_easing_changed(self, index):
+        """Handle changes to the zoom easing type combo.
+        
+        .. note:: This is called on changes to the zoom easing combo.
+
+        .. versionadded:: 1.0
+
+        :param index: Index of the now selected combo item.
+        :type flag: int
+
+        """
+        easing = QEasingCurve.Type(index)
+        self.zoom_easing_preview_animation.setEasingCurve(easing)
+
+
 
     def accept(self):
         """Process the animation sequence.
