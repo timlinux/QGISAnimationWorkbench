4c8de8efcf8529c7071579a449522dff1e000849
Tim Sutton <tim@kartoza.com.com>
2022-03-08 23:47:37 +0000

    Refactor updates

--- animation_workbench.py
+++ animation_workbench.py
@@ -31,8 +31,6 @@ from qgis.core import (
     QgsPointXY,
     QgsExpressionContextUtils,
     QgsProject,
-    QgsExpressionContextScope,
-    QgsMapRendererTask,
     QgsApplication,
     QgsCoordinateReferenceSystem,
     QgsMapRendererCustomPainterJob,
@@ -222,21 +220,6 @@ class AnimationWorkbench(QtWidgets.QDialog, FORM_CLASS):
         image = self.render_image()
         pixmap = QtGui.QPixmap.fromImage(image)
         self.current_frame_preview.setPixmap(pixmap)
-        # The maximum number of concurrent threads to allow
-        # during rendering. Probably setting to the same number 
-        # of CPU cores you have would be a good conservative approach
-        # You could probably run 100 or more on a decently specced machine
-        self.render_thread_pool_size = int(setting(
-            key='render_thread_pool_size', default=100))
-        # A list of tasks that need to be rendered but
-        # cannot be because the job queue is too full.
-        # we pop items off this list self.render_thread_pool_size
-        # at a time whenver the task manager tells us the queue is
-        # empty.
-        self.renderer_queue = []
-        # Queue manager for above.
-        QgsApplication.taskManager().allTasksFinished.connect(
-            self.process_more_tasks)
 
         self.progress_bar.setValue(0)
         # This will be half the number of frames per feature
@@ -318,36 +301,6 @@ class AnimationWorkbench(QtWidgets.QDialog, FORM_CLASS):
         self.output_directory = os.path.dirname(file_path) 
         self.output_file = file_path
         self.file_edit.setText(file_path)
-
-    def process_more_tasks(self):
-        """
-        Feed the QgsTaskManager with another bundle of tasks.
-
-        This slot is called whenever the QgsTaskManager queue is 
-        finished (by means of the 'allTasksFinished' signal).
-
-        :returns: None
-        """
-        self.total_tasks_lcd.display(self.total_frame_count)
-        self.completed_tasks_lcd.display(
-            self.total_frame_count - len(self.renderer_queue))        
-        if len(self.renderer_queue) == 0:
-            # all processing done so go off and generate
-            # the vid or gif
-            self.show_status()
-            self.processing_completed()
-            self.progress_bar.setValue(0)
-        else:
-            self.output_log_text_edit.append(
-                'Thread pool emptied, adding more tasks')
-            pop_size = self.render_thread_pool_size
-            if len(self.renderer_queue) < pop_size:
-                pop_size = len(self.renderer_queue)
-            for task in range(0, pop_size):
-                task_id = QgsApplication.taskManager().addTask(
-                    self.renderer_queue.pop(0))
-            self.progress_bar.setValue(
-                self.progress_bar.value() * pop_size)
 
     def display_information_message_box(
             self, parent=None, title=None, message=None):
@@ -638,56 +591,6 @@ class AnimationWorkbench(QtWidgets.QDialog, FORM_CLASS):
         job.renderSynchronously()
         painter.end()
         image.save(name)
-
-
-    def render_image_as_task(
-        self,
-        name,
-        current_feature_id,
-        current_frame,
-        action='None'):
-           
-        #size = self.iface.mapCanvas().size()
-        settings = self.iface.mapCanvas().mapSettings()
-        # The next part sets project variables that you can use in your 
-        # cartography etc. to see the progress. Here is an example
-        # of a QGS expression you can use in the map decoration copyright
-        # widget to show current script progress
-        # [%'Frame ' || to_string(coalesce(@current_frame, 0)) || '/' || 
-        # to_string(coalesce(@frames_per_feature, 0)) || ' for feature ' || 
-        # to_string(coalesce(@current_feature_id,0))%]
-        task_scope = QgsExpressionContextScope()
-        task_scope.setVariable('current_feature_id', current_feature_id)
-        task_scope.setVariable('frames_per_feature', self.frames_per_feature)
-        task_scope.setVariable('current_frame_for_feature', current_frame)        
-        task_scope.setVariable('current_animation_action', action)     
-        task_scope.setVariable('current_frame', self.image_counter)        
-        task_scope.setVariable('total_frame_count', self.total_frame_count)     
-
-        context = settings.expressionContext()
-        context.appendScope(task_scope) 
-        settings.setExpressionContext(context)
-        # Set the output file name for the render task
-        mapRendererTask = QgsMapRendererTask( settings, name, "PNG" )
-        # We need to clone the annotations because otherwise SIP will 
-        # pass ownership and then cause a crash when the render task is destroyed
-        annotations = QgsProject.instance().annotationManager().annotations()
-        annotations_list = [a.clone() for a in annotations]
-        if (len(annotations_list) > 0):
-            mapRendererTask.addAnnotations([a.clone() for a in annotations])
-        # Add decorations to the render job
-        decorations = self.iface.activeDecorations()
-        mapRendererTask.addDecorations(decorations)
-
-        # If we have reached the rendering pool cap, we will just keep 
-        # this task in a separate queue and then pop them off the queue
-        # self.render_thread_pool_size at a time whenver the task manager
-        # lets us know we have nothing to do
-        if QgsApplication.taskManager().countActiveTasks() > self.render_thread_pool_size:
-            self.renderer_queue.append(mapRendererTask)
-        else:
-            # Start the rendering task on the queue
-            task_id = QgsApplication.taskManager().addTask(mapRendererTask)
 
     def fly_feature_to_feature(self, start_feature, end_feature):
        
