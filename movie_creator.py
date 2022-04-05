621b6f6199aa361f488df278fb8bf467708de918
Tim Sutton <tim@kartoza.com.com>
2022-04-05 08:10:12 +0100

    WIP to fix soundtrack writing

--- movie_creator.py
+++ movie_creator.py
@@ -6,23 +6,15 @@ __license__ = "GPL version 3"
 __email__ = "tim@kartoza.com"
 __revision__ = "$Format:%H$"
 
-# This will make the QGIS use a world projection and then move the center
-# of the CRS sequentially to create a spinning globe effect
+import os
 from enum import Enum
 from typing import List, Optional
 
 # This import is to enable SIP API V2
 # noinspection PyUnresolvedReferences
 import qgis  # NOQA
-from qgis.PyQt.QtCore import (
-    pyqtSignal,
-    QProcess
-)
-from qgis.core import (
-    QgsTask,
-    QgsBlockingProcess,
-    QgsFeedback
-)
+from qgis.PyQt.QtCore import pyqtSignal, QProcess
+from qgis.core import QgsTask, QgsBlockingProcess, QgsFeedback
 
 from .utilities import get_ui_class, which
 
@@ -41,13 +33,13 @@ class MovieCreationTask(QgsTask):
     movie_created = pyqtSignal(str)
 
     def __init__(
-            self,
-            output_file: str,
-            music_file: str,
-            output_format: MovieFormat,
-            work_directory: str,
-            frame_filename_prefix: str,
-            framerate: int,
+        self,
+        output_file: str,
+        music_file: str,
+        output_format: MovieFormat,
+        work_directory: str,
+        frame_filename_prefix: str,
+        framerate: int,
     ):
         super().__init__("Exporting Movie", QgsTask.Flag.CanCancel)
 
@@ -62,30 +54,36 @@ class MovieCreationTask(QgsTask):
 
     def run_process(self, command: str, arguments: List[str]):
 
-        self.message.emit(f"Generating Movie: " + command + ' ' + ' '.join(arguments))
+        self.message.emit(
+            f"Generating Movie: " + command + " " + " ".join(arguments)
+        )
 
         def on_stdout(ba):
-            val = ba.data().decode('UTF-8')
+            val = ba.data().decode("UTF-8")
 
             on_stdout.buffer += val
-            if on_stdout.buffer.endswith('\n') or on_stdout.buffer.endswith('\r'):
+            if on_stdout.buffer.endswith("\n") or on_stdout.buffer.endswith(
+                "\r"
+            ):
                 # flush buffer
                 self.message.emit(on_stdout.buffer.rstrip())
-                on_stdout.buffer = ''
+                on_stdout.buffer = ""
 
         on_stdout.progress = 0
-        on_stdout.buffer = ''
+        on_stdout.buffer = ""
 
         def on_stderr(ba):
-            val = ba.data().decode('UTF-8')
+            val = ba.data().decode("UTF-8")
             on_stderr.buffer += val
 
-            if on_stderr.buffer.endswith('\n') or on_stderr.buffer.endswith('\r'):
+            if on_stderr.buffer.endswith("\n") or on_stderr.buffer.endswith(
+                "\r"
+            ):
                 # flush buffer
                 self.message.emit(on_stderr.buffer.rstrip())
-                on_stderr.buffer = ''
+                on_stderr.buffer = ""
 
-        on_stderr.buffer = ''
+        on_stderr.buffer = ""
 
         proc = QgsBlockingProcess(command, arguments)
         proc.setStdOutHandler(on_stdout)
@@ -93,17 +91,20 @@ class MovieCreationTask(QgsTask):
 
         res = proc.run(self.feedback)
         if self.feedback.isCanceled() and res != 0:
-            self.message.emit('Process was canceled and did not complete')
-        elif not self.feedback.isCanceled() and proc.exitStatus() == QProcess.CrashExit:
-            self.message.emit('Process was unexpectedly terminated')
+            self.message.emit("Process was canceled and did not complete")
+        elif (
+            not self.feedback.isCanceled()
+            and proc.exitStatus() == QProcess.CrashExit
+        ):
+            self.message.emit("Process was unexpectedly terminated")
         elif res == 0:
-            self.message.emit('Process completed successfully')
+            self.message.emit("Process completed successfully")
         elif proc.processError() == QProcess.FailedToStart:
             self.message.emit(
-                'Process {} failed to start. Either {} is missing, or you may have insufficient permissions to run the program.').format(
-                command, command)
+                "Process {} failed to start. Either {} is missing, or you may have insufficient permissions to run the program."
+            ).format(command, command)
         else:
-            self.message.emit('Process returned error code {}'.format(res))
+            self.message.emit("Process returned error code {}".format(res))
 
     def run(self):
         """
@@ -123,13 +124,17 @@ class MovieCreationTask(QgsTask):
             # ImageMagick) is correct...
             # delay of 3.33 makes the output around 30fps
 
-            self.run_process(convert,
-                             [
-                                 '-delay', str(100 / self.framerate),
-                                 '-loop' ,'0',
-                                 f'{self.work_directory}/{self.frame_filename_prefix}-*.png',
-                                 self.output_file
-                             ])
+            self.run_process(
+                convert,
+                [
+                    "-delay",
+                    str(100 / self.framerate),
+                    "-loop",
+                    "0",
+                    f"{self.work_directory}/{self.frame_filename_prefix}-*.png",
+                    self.output_file,
+                ],
+            )
 
             # Now do a second pass with image magick to resize and compress the
             # gif as much as possible.  The remap option basically takes the
@@ -137,20 +142,26 @@ class MovieCreationTask(QgsTask):
             # on you cartography you may also want to bump up the colors param
             # to increase palette size and of course adjust the scale factor to
             # the ultimate image size you want
-            self.run_process(convert,
-                             [
-                                 self.output_file,
-                                 '-coalesce',
-                                 '-scale' ,'600x600',
-                                 '-fuzz', '2%', '+dither',
-                                 '-remap' ,f'{self.output_file}[20]',
-                                 '+dither',
-                                 '-colors', '14',
-                                 '-layers',
-                                 'Optimize',
-                                 f'{self.work_directory}/animation_small.gif'
-                             ]
-                             )
+            self.run_process(
+                convert,
+                [
+                    self.output_file,
+                    "-coalesce",
+                    "-scale",
+                    "600x600",
+                    "-fuzz",
+                    "2%",
+                    "+dither",
+                    "-remap",
+                    f"{self.output_file}[20]",
+                    "+dither",
+                    "-colors",
+                    "14",
+                    "-layers",
+                    "Optimize",
+                    f"{self.work_directory}/animation_small.gif",
+                ],
+            )
 
             self.message.emit(f"GIF written to {self.output_file}")
             self.movie_created.emit(self.output_file)
@@ -167,22 +178,34 @@ class MovieCreationTask(QgsTask):
             self.message.emit(f"ffmpeg found: {ffmpeg}")
 
             arguments = [
-                '-y',
-                f'-framerate', str(self.framerate),
-                '-pattern_type', 'glob',
-                '-i', f"{self.work_directory}/{self.frame_filename_prefix}-*.png"
+                "-y",
+                f"-framerate",
+                str(self.framerate),
+                "-pattern_type",
+                "glob",
+                "-i",
+                f"{self.work_directory}/{self.frame_filename_prefix}-*.png",
+                "-vf",
+                "pad=ceil(iw/2)*2:ceil(ih/2)*2:color=white",
+                "-c:v",
+                "libx264",
+                "-pix_fmt",
+                "yuv420p",
             ]
-
             if self.music_file:
-                arguments.append(f"-i {self.music_file}")
+                arguments.extend(self.output_file + "_")
 
-            arguments.extend([
-                '-vf', 'pad=ceil(iw/2)*2:ceil(ih/2)*2:color=white',
-                '-c:v', 'libx264',
-                '-pix_fmt', 'yuv420p',
-                self.output_file
-            ])
+                self.run_process(ffmpeg, arguments)
+                self.message.emit(
+                    f"MP4 without soundtrack written to {self.output_file}_"
+                )
+            else:
+                arguments.extend(self.output_file)
 
+                self.run_process(ffmpeg, arguments)
+                self.message.emit(
+                    f"MP4 without soundtrack written to {self.output_file}"
+                )
             # windows_command = ("""
             #    %s -y -framerate %s -pattern_type sequence \
             #    -start_number 0000000001 \
@@ -195,10 +218,31 @@ class MovieCreationTask(QgsTask):
             #    self.frame_filename_prefix,
             #    self.output_file))
 
-            self.run_process(ffmpeg, arguments)
-            self.message.emit(f"MP4 written to {self.output_file}")
-            self.movie_created.emit(self.output_file)
+            # If there is a music file, we do a second pass and add the
+            # file into the video container. From my testing on the CLI,
+            # this works more smoothly and doesnt have issues like
+            # video blanking that doing it in one pass does.
 
+            if self.music_file:
+                self.message.emit(f"Adding sound track to video container")
+
+                arguments = [
+                    "-y",
+                    "-i",
+                    f"{self.output_file}",
+                    "-i",
+                    f"{self.music_file}",
+                    "-c",
+                    "copy",  # Will copy the sound into the video container
+                    f"{self.output_file}_",
+                ]
+
+                self.run_process(ffmpeg, arguments)
+                self.message.emit(
+                    f"MP4 with sountrack written to {self.output_file}"
+                )
+
+        self.movie_created.emit(self.output_file)
         self.feedback = None
         return True
 
