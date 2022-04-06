e2334f0741fc980d4c613f48b8edb88ab94e173d
Nyall Dawson <nyall.dawson@gmail.com>
2022-04-06 15:34:16 +1000

    Add test for movie command generation

--- animation_workbench/core/movie_creator.py
+++ animation_workbench/core/movie_creator.py
@@ -7,10 +7,9 @@ __email__ = "tim@kartoza.com"
 __revision__ = "$Format:%H$"
 
 import os
-import shutil
 import tempfile
 from enum import Enum
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 from qgis.PyQt.QtCore import pyqtSignal, QProcess
 from qgis.core import QgsTask, QgsBlockingProcess, QgsFeedback
@@ -24,6 +23,155 @@ class MovieFormat(Enum):
     """
     GIF = 0
     MP4 = 1
+
+
+class MovieCommandGenerator:
+    """
+    Generates the command line strings for movie creation
+    """
+
+    def __init__(
+            self,
+            output_file: str,
+            music_file: Optional[str],
+            output_format: MovieFormat,
+            work_directory: str,
+            frame_filename_prefix: str,
+            framerate: int,
+            temp_dir: str
+    ):
+        self.output_file = output_file
+        self.music_file = music_file
+        self.format = output_format
+        self.work_directory = work_directory
+        self.frame_filename_prefix = frame_filename_prefix
+        self.framerate = framerate
+        self.temp_dir = temp_dir
+
+    def as_commands(self) -> List[Tuple[str, List]]:
+        """
+        Returns a list of commands necessary for the movie generation (as tuples of the command and arguments list)
+        """
+        res = []
+        if self.format == MovieFormat.GIF:
+            convert = CoreUtils.which("convert")[0]
+
+            # First generate the GIF. If this fails try to run the call from
+            # the command line and check the path to convert (provided by
+            # ImageMagick) is correct...
+            # delay of 3.33 makes the output around 30fps
+
+            res.append((
+                convert,
+                [
+                    "-delay",
+                    str(100 / self.framerate),
+                    "-loop",
+                    "0",
+                    f"{self.work_directory}/{self.frame_filename_prefix}-*.png",
+                    self.output_file,
+                ]
+            )
+            )
+
+            # Now do a second pass with image magick to resize and compress the
+            # gif as much as possible.  The remap option basically takes the
+            # first image as a reference image for the colour palette Depending
+            # on you cartography you may also want to bump up the colors param
+            # to increase palette size and of course adjust the scale factor to
+            # the ultimate image size you want
+            res.append((
+                convert,
+                [
+                    self.output_file,
+                    "-coalesce",
+                    "-scale",
+                    "600x600",
+                    "-fuzz",
+                    "2%",
+                    "+dither",
+                    "-remap",
+                    f"{self.output_file}[20]",
+                    "+dither",
+                    "-colors",
+                    "14",
+                    "-layers",
+                    "Optimize",
+                    f"{self.work_directory}/animation_small.gif",
+                ])
+            )
+        else:
+            ffmpeg = CoreUtils.which("ffmpeg")[0]
+            # Also, we will make a video of the scene - useful for cases where
+            # you have a larger colour palette and gif will not hack it.
+            # The Pad option is to deal with cases where ffmpeg complains
+            # because the h or w of the image is an odd number of pixels.
+            # color=white pads the video with white pixels.
+            # Change to black if needed.
+            # -y to force overwrite exising file
+
+            arguments = [
+                "-y",
+                "-framerate",
+                str(self.framerate),
+                "-pattern_type",
+                "glob",
+                "-i",
+                f"{self.work_directory}/{self.frame_filename_prefix}-*.png",
+                "-vf",
+                "pad=ceil(iw/2)*2:ceil(ih/2)*2:color=white",
+                "-c:v",
+                "libx264",
+                "-pix_fmt",
+                "yuv420p",
+            ]
+
+            if not self.music_file:
+                arguments.append(self.output_file)
+                res.append((ffmpeg, arguments))
+            else:
+                temp_video_path = str(
+                    os.path.join(self.temp_dir, "animation_workbench.mp4")
+                )
+                arguments.append(temp_video_path)
+                # This will build the base vide with no soundtrack
+                # in the above temporary folder
+                res.append((ffmpeg, arguments))
+
+                # windows_command = ("""
+                #    %s -y -framerate %s -pattern_type sequence \
+                #    -start_number 0000000001 \
+                #    -i "%s/%s-%00000000010d.png" -vf \
+                #    "pad=ceil(iw/2)*2:ceil(ih/2)*2:color=white" \
+                #    -c:v libx264 -pix_fmt yuv420p %s""" % (
+                #    ffmpeg,
+                #    framerate,
+                #    self.work_directory,
+                #    self.frame_filename_prefix,
+                #    self.output_file))
+
+                # If there is a music file, we do a second pass and add the
+                # file into the video container. From my testing on the CLI,
+                # this works more smoothly and doesn't have issues like
+                # video blanking that doing it in one pass does.
+
+                arguments = [
+                    "-y",
+                    "-i",
+                    f"{temp_video_path}",
+                    "-i",
+                    f"{self.music_file}",
+                    "-c",
+                    # Will copy the sound into the video container
+                    "copy",
+                    # will truncate output to shortest between vid and audio
+                    "-shortest",
+                    f"{self.output_file}",
+                ]
+
+                res.append((ffmpeg, arguments))
+
+        return res
 
 
 class MovieCreationTask(QgsTask):
@@ -119,137 +267,28 @@ class MovieCreationTask(QgsTask):
 
         if self.format == MovieFormat.GIF:
             self.message.emit("Generating GIF")
-
             convert = CoreUtils.which("convert")[0]
             self.message.emit(f"convert found: {convert}")
-
-            # Now generate the GIF. If this fails try to run the call from
-            # the command line and check the path to convert (provided by
-            # ImageMagick) is correct...
-            # delay of 3.33 makes the output around 30fps
-
-            self.run_process(
-                convert,
-                [
-                    "-delay",
-                    str(100 / self.framerate),
-                    "-loop",
-                    "0",
-                    f"{self.work_directory}/{self.frame_filename_prefix}-*.png",
-                    self.output_file,
-                ],
-            )
-
-            # Now do a second pass with image magick to resize and compress the
-            # gif as much as possible.  The remap option basically takes the
-            # first image as a reference image for the colour palette Depending
-            # on you cartography you may also want to bump up the colors param
-            # to increase palette size and of course adjust the scale factor to
-            # the ultimate image size you want
-            self.run_process(
-                convert,
-                [
-                    self.output_file,
-                    "-coalesce",
-                    "-scale",
-                    "600x600",
-                    "-fuzz",
-                    "2%",
-                    "+dither",
-                    "-remap",
-                    f"{self.output_file}[20]",
-                    "+dither",
-                    "-colors",
-                    "14",
-                    "-layers",
-                    "Optimize",
-                    f"{self.work_directory}/animation_small.gif",
-                ],
-            )
-
-            self.message.emit(f"GIF written to {self.output_file}")
-            self.movie_created.emit(self.output_file)
         else:
             self.message.emit("Generating MP4 Movie")
             ffmpeg = CoreUtils.which("ffmpeg")[0]
-            # Also, we will make a video of the scene - useful for cases where
-            # you have a larger colour palette and gif will not hack it.
-            # The Pad option is to deal with cases where ffmpeg complains
-            # because the h or w of the image is an odd number of pixels.
-            # color=white pads the video with white pixels.
-            # Change to black if needed.
-            # -y to force overwrite exising file
             self.message.emit(f"ffmpeg found: {ffmpeg}")
 
-            arguments = [
-                "-y",
-                "-framerate",
-                str(self.framerate),
-                "-pattern_type",
-                "glob",
-                "-i",
-                f"{self.work_directory}/{self.frame_filename_prefix}-*.png",
-                "-vf",
-                "pad=ceil(iw/2)*2:ceil(ih/2)*2:color=white",
-                "-c:v",
-                "libx264",
-                "-pix_fmt",
-                "yuv420p",
-            ]
+        # This will create a temporary working dir & filename
+        # that is secure and clean up after itself.
+        with tempfile.TemporaryDirectory() as tmp:
+            generator = MovieCommandGenerator(
+                output_file=self.output_file,
+                music_file=self.music_file,
+                output_format=self.format,
+                work_directory=self.work_directory,
+                frame_filename_prefix=self.frame_filename_prefix,
+                framerate=self.framerate,
+                temp_dir=tmp
+            )
 
-            # This will create a temporary working dir & filename
-            # that is secure and clean up after itself.
-            with tempfile.TemporaryDirectory() as tmp:
-                temp_video_path = str(
-                    os.path.join(tmp, "animation_workbench.mp4")
-                )
-                arguments.append(temp_video_path)
-                # This will build the base vide with no soundtrack
-                # in the above temporary folder
-                self.run_process(ffmpeg, arguments)
-                # windows_command = ("""
-                #    %s -y -framerate %s -pattern_type sequence \
-                #    -start_number 0000000001 \
-                #    -i "%s/%s-%00000000010d.png" -vf \
-                #    "pad=ceil(iw/2)*2:ceil(ih/2)*2:color=white" \
-                #    -c:v libx264 -pix_fmt yuv420p %s""" % (
-                #    ffmpeg,
-                #    framerate,
-                #    self.work_directory,
-                #    self.frame_filename_prefix,
-                #    self.output_file))
-
-                # If there is a music file, we do a second pass and add the
-                # file into the video container. From my testing on the CLI,
-                # this works more smoothly and doesn't have issues like
-                # video blanking that doing it in one pass does.
-
-                if self.music_file:
-                    self.message.emit("Adding sound track to video container")
-
-                    arguments = [
-                        "-y",
-                        "-i",
-                        f"{temp_video_path}",
-                        "-i",
-                        f"{self.music_file}",
-                        "-c",
-                        # Will copy the sound into the video container
-                        "copy",
-                        # will truncate output to shortest between vid and audio
-                        "-shortest",
-                        f"{self.output_file}",
-                    ]
-
-                    self.run_process(ffmpeg, arguments)
-                    self.message.emit(
-                        f"MP4 with soundtrack written to {self.output_file}"
-                    )
-                else:
-                    shutil.copyfile(temp_video_path, self.output_file)
-                    self.message.emit(
-                        f"MP4 without soundtrack written to {self.output_file}"
-                    )
+            for command, arguments in generator.as_commands():
+                self.run_process(command, arguments)
 
         self.movie_created.emit(self.output_file)
         self.feedback = None
---
+++ animation_workbench/test/test_movie_creator.py
@@ -0,0 +1,148 @@
+# coding=utf-8
+"""Movie creator test
+
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+"""
+
+__author__ = '(C) 2018 by Nyall Dawson'
+__date__ = '20/04/2018'
+__copyright__ = 'Copyright 2018, North Road'
+# This will get replaced with a git SHA1 when you do a git archive
+__revision__ = '$Format:%H$'
+
+import unittest
+
+from animation_workbench.core import (
+    MovieCommandGenerator,
+    MovieFormat
+)
+from .utilities import get_qgis_app
+
+QGIS_APP = get_qgis_app()
+
+
+class MovieCreatorTest(unittest.TestCase):
+    """Test MovieCommandGenerator works."""
+
+    # pylint: disable=too-many-statements
+
+    def test_mp4(self):
+        """
+        Test mp4 command generation
+        """
+        generator = MovieCommandGenerator(
+            output_file='/home/me/videos/test.mp4',
+            music_file=None,
+            output_format=MovieFormat.MP4,
+            work_directory='/tmp/movies',
+            frame_filename_prefix='frames',
+            framerate=90,
+            temp_dir='/tmp/temp'
+        )
+
+        commands = generator.as_commands()
+        self.assertEqual(commands, [('/usr/bin/ffmpeg',
+                                     ['-y',
+                                      '-framerate',
+                                      '90',
+                                      '-pattern_type',
+                                      'glob',
+                                      '-i',
+                                      '/tmp/movies/frames-*.png',
+                                      '-vf',
+                                      'pad=ceil(iw/2)*2:ceil(ih/2)*2:color=white',
+                                      '-c:v',
+                                      'libx264',
+                                      '-pix_fmt',
+                                      'yuv420p',
+                                      '/home/me/videos/test.mp4'])])
+
+    def test_mp4_with_music(self):
+        """
+        Test mp4 command generation
+        """
+        generator = MovieCommandGenerator(
+            output_file='/home/me/videos/test.mp4',
+            music_file='/home/me/music/lalala.mp3',
+            output_format=MovieFormat.MP4,
+            work_directory='/tmp/movies',
+            frame_filename_prefix='frames',
+            framerate=90,
+            temp_dir='/tmp/temp'
+        )
+
+        commands = generator.as_commands()
+        self.assertEqual(commands, [('/usr/bin/ffmpeg',
+                                     ['-y',
+                                      '-framerate',
+                                      '90',
+                                      '-pattern_type',
+                                      'glob',
+                                      '-i',
+                                      '/tmp/movies/frames-*.png',
+                                      '-vf',
+                                      'pad=ceil(iw/2)*2:ceil(ih/2)*2:color=white',
+                                      '-c:v',
+                                      'libx264',
+                                      '-pix_fmt',
+                                      'yuv420p',
+                                      '/tmp/temp/animation_workbench.mp4']),
+                                    ('/usr/bin/ffmpeg',
+                                     ['-y',
+                                      '-i',
+                                      '/tmp/temp/animation_workbench.mp4',
+                                      '-i',
+                                      '/home/me/music/lalala.mp3',
+                                      '-c',
+                                      'copy',
+                                      '-shortest',
+                                      '/home/me/videos/test.mp4'])])
+
+    def test_gif(self):
+        """
+        Test gif command generation
+        """
+        generator = MovieCommandGenerator(
+            output_file='/home/me/videos/test.gif',
+            music_file=None,
+            output_format=MovieFormat.GIF,
+            work_directory='/tmp/movies',
+            frame_filename_prefix='frames',
+            framerate=90,
+            temp_dir='/tmp/temp'
+        )
+
+        commands = generator.as_commands()
+        self.assertEqual(commands, [('/usr/bin/convert',
+                                     ['-delay',
+                                      '1.1111111111111112',
+                                      '-loop',
+                                      '0',
+                                      '/tmp/movies/frames-*.png',
+                                      '/home/me/videos/test.gif']),
+                                    ('/usr/bin/convert',
+                                     ['/home/me/videos/test.gif',
+                                      '-coalesce',
+                                      '-scale',
+                                      '600x600',
+                                      '-fuzz',
+                                      '2%',
+                                      '+dither',
+                                      '-remap',
+                                      '/home/me/videos/test.gif[20]',
+                                      '+dither',
+                                      '-colors',
+                                      '14',
+                                      '-layers',
+                                      'Optimize',
+                                      '/tmp/movies/animation_small.gif'])])
+
+
+if __name__ == "__main__":
+    suite = unittest.makeSuite(MovieCreatorTest)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
