bcf73f2ba9f7ac67ccd5b485b347a3a8fc1cbbc1
Tim Sutton <tim@kartoza.com>
2022-05-27 17:03:48 +0100

    ✅ Fix test

--- animation_workbench/test/test_animation_controller.py
+++ animation_workbench/test/test_animation_controller.py
@@ -434,7 +434,7 @@ class AnimationControllerTest(unittest.TestCase):
         job = next(it)
         self.assertEqual(job.map_settings.frameRate(), 2)
         self.assertEqual(job.map_settings.currentFrame(), 0)
-        self.assertAlmostEqual(job.map_settings.scale(), 444444, delta=120000)
+        self.assertAlmostEqual(job.map_settings.scale(), 266666, delta=120000)
         self.assertAlmostEqual(job.map_settings.extent().center().x(), 1, 2)
         self.assertAlmostEqual(job.map_settings.extent().center().y(), 2, 2)
         self.assertAlmostEqual(
@@ -980,7 +980,7 @@ class AnimationControllerTest(unittest.TestCase):
         job = next(it)
         self.assertEqual(job.map_settings.frameRate(), 2)
         self.assertEqual(job.map_settings.currentFrame(), 0)
-        self.assertAlmostEqual(job.map_settings.scale(), 444444, delta=120000)
+        self.assertAlmostEqual(job.map_settings.scale(), 266666, delta=120000)
         self.assertAlmostEqual(job.map_settings.extent().center().x(), 1, 2)
         self.assertAlmostEqual(job.map_settings.extent().center().y(), 2, 2)
         self.assertAlmostEqual(
