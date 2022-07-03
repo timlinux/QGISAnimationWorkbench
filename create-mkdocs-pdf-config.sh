3a4d1509fcda7bcd5d29fc4d44553f5fcfca3516
Tim Sutton <tim@kartoza.com>
2022-07-03 12:47:36 +0100

    Refactor mkdocs build so there is a canonical config

---
+++ create-mkdocs-pdf-config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script will assemble a mkdocs.yml
+# file with plugins section suitable for 
+# PDF site generation.
+
+# This script is used both in manual
+# site compilation (via build-docs-html.sh)
+# and via the github workflow for 
+# publishing the site to github pages
+# .github/workflows/CompileMKDocsToPDF.yml
+
+cat mkdocs-base.yml > mkdocs.yml
+cat mkdocs-pdf-yml >> mkdocs.yml
