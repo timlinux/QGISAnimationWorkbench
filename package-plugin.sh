8a8d08a4bc35d732b1853cf6c4fcd466ed01191e
Tim Sutton <tim@kartoza.com>
2024-04-17 13:53:37 +0100

    Bump version

--- package-plugin.sh
+++ package-plugin.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+
+version=$(grep '^version=' animation_workbench/metadata.txt | cut -d '=' -f 2| tr -d '\n')
+echo $version
+version="1.2"
+rm animation_workbench*.zip
 cd animation_workbench
 rm -rf __pycache__ core/__pycache__/ gui/__pycache__/
-zip -qq -r ../animation_workbench.zip *
 cd ..
-ls -lah animation_workbench.zip
+zip -r animation_workbench-${version}.zip animation_workbench
+cd ..
+ls -lah animation_workbench-${version}.zip
