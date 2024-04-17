8e9903c15ab9d8b8fd0b1a7132499b30c42ba231
Tim Sutton <tim@kartoza.com>
2024-04-17 10:22:20 +0100

    Added some house keeping files

---
+++ vscode.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+echo "🪛 Installing VSCode Extensions:"
+echo "--------------------------------"
+code --extensions-dir=".vscode-extensions" --install-extension donjayamanne.python-environment-manager
+code --extensions-dir=".vscode-extensions" --install-extension donjayamanne.python-extension-pack
+code --extensions-dir=".vscode-extensions" --install-extension hbenl.vscode-test-explorer
+code --extensions-dir=".vscode-extensions" --install-extension jamesqquick.python-class-generator
+code --extensions-dir=".vscode-extensions" --install-extension KevinRose.vsc-python-indent
+code --extensions-dir=".vscode-extensions" --install-extension littlefoxteam.vscode-python-test-adapter
+code --extensions-dir=".vscode-extensions" --install-extension ms-python.debugpy
+code --extensions-dir=".vscode-extensions" --install-extension ms-python.python
+code --extensions-dir=".vscode-extensions" --install-extension ms-python.vscode-pylance
+code --extensions-dir=".vscode-extensions" --install-extension ms-vscode.test-adapter-converter
+code --extensions-dir=".vscode-extensions" --install-extension njpwerner.autodocstring
+code --extensions-dir=".vscode-extensions" --install-extension VisualStudioExptTeam.intellicode-api-usage-examples
+code --extensions-dir=".vscode-extensions" --install-extension VisualStudioExptTeam.vscodeintellicode
+
+code --extensions-dir=".vscode-extensions" .
