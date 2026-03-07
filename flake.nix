84d2e459eafbc69184b42bf1bc871c5481860480
Tim Sutton <tim@kartoza.com>
2026-03-07 12:31:32 +0000

    chore: apply tailor for consistent repo configuration
    Adds community health files, GitHub configuration, and automated
    maintenance workflows using tailor (https://github.com/wimpysworld/tailor)

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Nix flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              gh
+              jq
+              just
+              yq
+            ];
+          };
+        }
+      );
+    };
+}
